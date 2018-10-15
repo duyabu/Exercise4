d1348bb49362ab6ecd0952f92db0d7747818d7e0
duyabu <phmngcduy@yahoo.com>
2018-10-15 17:18:47 +0700

    add 3 tests

--- ConsoleApplication1/UnitTestProject1/UnitTest1.cs
+++ ConsoleApplication1/UnitTestProject1/UnitTest1.cs
@@ -9,14 +9,49 @@ namespace UnitTestProject1
         [TestMethod]
         public void TestIsOdd()
         {
+            bool result_0 = Program.IsOdd(0);
+            bool result_1 = Program.IsOdd(1);
+            bool result_2 = Program.IsOdd(2);
+            bool result_123 = Program.IsOdd(123);
+            bool result_456 = Program.IsOdd(456);
+
+            Assert.AreEqual(result_0, false);
+            Assert.AreEqual(result_1, true);
+            Assert.AreEqual(result_2, false);
+            Assert.AreEqual(result_123, true);
+            Assert.AreEqual(result_456, false);
         }
         [TestMethod]
         public void TestIsEven()
         {
+            bool result_0 = Program.IsOdd(0);
+            bool result_1 = Program.IsOdd(1);
+            bool result_2 = Program.IsOdd(2);
+            bool result_123 = Program.IsOdd(123);
+            bool result_456 = Program.IsOdd(456);
+
+            Assert.AreEqual(result_0, true);
+            Assert.AreEqual(result_1, false);
+            Assert.AreEqual(result_2, true);
+            Assert.AreEqual(result_123, false);
+            Assert.AreEqual(result_456, true);
         }
         [TestMethod]
         public void TestIsPrime()
         {
+            bool result_0 = Program.IsPrime(0);
+            bool result_1 = Program.IsPrime(1);
+            bool result_2 = Program.IsPrime(2);
+            bool result_3 = Program.IsPrime(3);
+            bool result_13 = Program.IsPrime(13);
+            bool result_14 = Program.IsPrime(14);
+
+            Assert.AreEqual(result_0, false);
+            Assert.AreEqual(result_1, false);
+            Assert.AreEqual(result_2, true);
+            Assert.AreEqual(result_3, true);
+            Assert.AreEqual(result_13, true);
+            Assert.AreEqual(result_14, false);
         }
         [TestMethod]
         public void TestSquare()
